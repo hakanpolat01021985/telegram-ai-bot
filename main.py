99d170bb4362b3c12755e97d0317cdae8d4d3f1e
hakanpolat01021985 <hakanpolat01021985@gmail.com>
2025-08-30 12:48:07 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ import openai
 from telegram.ext import Updater, MessageHandler, Filters
 
 # Ortam değişkenlerinden tokenleri al
-TELEGRAM_TOKEN = os.environ["TELEGRAM_TOKEN"]
-OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
+TELEGRAM_TOKEN = os.environ["8490762605:AAHNFyHkOEZhgsFReyRKOSQ8exDSlgQnEwY"]
+OPENAI_API_KEY = os.environ["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"]
+
 
 openai.api_key = OPENAI_API_KEY
 
